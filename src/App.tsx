b7faf443201cb926c400e9840da1f260a3c6711a
luishsilva <luizhrick@gmail.com>
2024-05-20 08:05:55 -0600

    Update favorite and unfavorite dog optmistic

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,31 @@ import { Requests } from "./api";
 import { Section } from "./Components/Section";
 import { Dogs } from "./Components/Dogs";
 import { Dog } from "./types";
+import Toast from "react-hot-toast";
 
 export function App() {
 
-  const [allDogs, setAllDogs] = useState<Dog[]>([])
+  const [dogs, setDogs] = useState<Dog[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   useEffect(() => {
     refetchData();
   }, []); 
 
-  const refetchData = () => { 
-    Requests.getAllDogs().then(setAllDogs).catch(error => { console.error('Error fecthing dogs:', error)})
+  const refetchData = () => {
+    Requests.getAllDogs()
+    .then(setDogs)
+    .catch(error => { 
+      Toast.error('Error fetching dogs:', error);
+    })
+  }
+
+  const updateDog = (id: number, isFavorite: boolean) => {
+    setDogs(
+      dogs.map((dog) => 
+        dog.id === id ? {...dog, isFavorite: isFavorite} : dog 
+      )
+    );
   }
 
   return (
@@ -22,7 +36,7 @@ export function App() {
         <h1>pup-e-picker (Functional)</h1>
       </header>
       <Section label={"Dogs: "}>
-        <Dogs allDogs={allDogs}/>
+        <Dogs dogs={dogs} updateDog={updateDog}/>
       </Section>
     </div>
   );
