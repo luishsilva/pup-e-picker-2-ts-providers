0e9060de50ea560aed38126aee64bf6fc644b7f9
luishsilva <luizhrick@gmail.com>
2024-05-22 07:19:21 -0600

    Remove unnecessary methods loading data from DogsProvider and execute actions click

--- src/Components/Dogs.tsx
+++ src/Components/Dogs.tsx
@@ -1,51 +1,54 @@
 // Right now these dogs are constant, but in reality we should be getting these from our server
 // Todo: Refactor to get rid of props (THERE SHOULD BE NO PROPS DRILLING ON THIS COMPONENT)
 import { DogCard } from "./DogCard";
-import { Dog } from "../types";
+import { useDogs } from "./providers/DogsProvider";
+import { toast } from "react-hot-toast";
 
-export const Dogs = (
-  { 
-    deleteDog,
-    dogs, 
-    updateDog,
-  } 
-  : 
-  {
-    deleteDog: (id: number) => Promise<void>,
-    dogs: Dog[], 
-    updateDog: (id: number, isFavorite: boolean) => Promise<void>;
-  }) =>
-  // no props allowed
-  {
-    return (
-      //  the "<> </>"" are called react fragments, it's like adding all the html inside
-      // without adding an actual html element
-      <>
-        {/* Make all the dog cards show up here */}
-        {
-          dogs.length > 0 && dogs.map(dog => (
-            <DogCard 
-              dog={{
-                id: dog.id,
-                image: dog.image,
-                description: dog.description,
-                isFavorite: dog.isFavorite,
-                name: dog.name,
+export const Dogs = () => {
+
+  const {activeTab, deleteDog,  dogs,  updateDog, isLoading, setIsLoading, } = useDogs();
+
+  const filteredDogs =
+    activeTab === 'all-dogs'
+      ? dogs
+      : dogs.filter(dog =>
+          activeTab == 'favorited' ? dog.isFavorite : !dog.isFavorite
+        );
+
+  return (
+    //  the "<> </>"" are called react fragments, it's like adding all the html inside
+    // without adding an actual html element
+    <>
+      {
+        filteredDogs.length > 0 && filteredDogs.map(dog => (
+          <DogCard 
+            dog={{
+              id: dog.id,
+              image: dog.image,
+              description: dog.description,
+              isFavorite: dog.isFavorite,
+              name: dog.name,
+            }}
+            key={dog.id}
+              onTrashIconClick={() => {
+                setIsLoading(true);
+                deleteDog(dog.id)
+                .catch(() => {
+                  toast.error('Failed to delete the Dog, Please try again.', {
+                    duration: 2000,
+                  });
+                }).finally(() => setIsLoading(false));
               }}
-              key={dog.id}
-                onTrashIconClick={() => {
-                  deleteDog(dog.id);
-                }}
-                onHeartClick={() => {
-                  updateDog(dog.id,false);
-                }}
-                onEmptyHeartClick={() => {
-                  updateDog(dog.id, true);
-                }}
-                isLoading={false}
-            />
-          ))
-        }
-      </>
-    );
-  };
+              onHeartClick={() => {
+                updateDog(dog.id,false);
+              }}
+              onEmptyHeartClick={() => {
+                updateDog(dog.id, true);
+              }}
+              isLoading={isLoading}
+          />
+        ))
+      }
+    </>
+  );
+};
