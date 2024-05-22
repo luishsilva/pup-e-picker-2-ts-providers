7f29d216ac82f4071e951b441951c059ec6bab3b
luishsilva <luizhrick@gmail.com>
2024-05-22 07:14:50 -0600

    Wrap app with DogProvider

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,13 @@ import "./App.css";
 import "./index.css";
 import { Toaster } from "react-hot-toast";
 import { App } from "./App";
+import { DogsProvider } from "./Components/providers/DogsProvider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Toaster />
-    <App />
+    <DogsProvider>
+      <App />
+    </DogsProvider>
   </React.StrictMode>
 );
